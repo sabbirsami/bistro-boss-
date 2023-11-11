171c945a3e284c94c2bf4e4f1eb3b84be8fba6b0
SabbirSami <smd71430@gmail.com>
2023-11-11 11:21:37 +0600

    add category section

---
+++ src/components/home/Category.jsx
@@ -0,0 +1,76 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import slide1 from "../../assets/home/slide1.jpg";
+import slide2 from "../../assets/home/slide2.jpg";
+import slide3 from "../../assets/home/slide3.jpg";
+import slide4 from "../../assets/home/slide4.jpg";
+import slide5 from "../../assets/home/slide5.jpg";
+
+const Category = () => {
+    return (
+        <div>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img
+                        className="w-full object-cover h"
+                        src={slide1}
+                        alt=""
+                    />
+                    <h3 className="text-3xl text-center -mt-20 pb-20">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="w-full object-cover h"
+                        src={slide2}
+                        alt=""
+                    />
+                    <h3 className="text-3xl text-center -mt-20 pb-20">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="w-full object-cover h"
+                        src={slide3}
+                        alt=""
+                    />
+                    <h3 className="text-3xl text-center -mt-20 pb-20">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="w-full object-cover h"
+                        src={slide4}
+                        alt=""
+                    />
+                    <h3 className="text-3xl text-center -mt-20 pb-20">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="w-full object-cover h"
+                        src={slide5}
+                        alt=""
+                    />
+                    <h3 className="text-3xl text-center -mt-20 pb-20">Pizza</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="w-full object-cover h"
+                        src={slide1}
+                        alt=""
+                    />
+                    <h3 className="text-3xl text-center -mt-20 pb-20">Pizza</h3>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Category;
